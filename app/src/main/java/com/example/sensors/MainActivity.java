af8af1ea0e93c1c956c8329b5460011517a24d34
Yago Garcia <yagog5002@gmail.com>
2025-01-30 19:07:53 +0100

    barometro check

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sensors;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+
+        if (pressureSensor != null) {
+            Toast.makeText(this, "TIENES BAROMETRO", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "GAYGAYGAYGAYGAY", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
